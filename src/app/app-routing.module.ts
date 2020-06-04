c79f5ebe62559eca539b726d4df5179a203e3c8d
Kmaad <kmaad@MacBook-Pro-de-Kmaad.local>
2020-06-04 17:50:53 +0200

    Main - main-page : Done !! ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'header', component: HeaderComponent },
+  { path: 'main-page', component: MainPageComponent },
+  { path: '', redirectTo:'main-page', pathMatch: 'full', }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
